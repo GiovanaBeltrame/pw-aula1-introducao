e87a6b026b44fab83d3c7a594e7b62081707f5ea
GiovanaBeltrame <giovanabeltrame49@gmail.com>
2021-03-17 13:14:40 +0000

    aula1-commit1

---
+++ fundamentos01/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import HelloWorld from './components/basic/HelloWorld'
+
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Aula 01 - Fundamentos 01</h1>
+
+                <HelloWorld />
+            </div>
+        )
+    }
+}
+
+export default App;
